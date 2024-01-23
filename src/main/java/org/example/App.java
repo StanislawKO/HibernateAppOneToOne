ae2b65df38ee112333569848471d3415d9b2c107
Stanislaav Kozh <printks@yandex.ru>
2024-01-23 21:16:12 +0300

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,37 @@
+package org.example;
+
+import org.example.model.Passport;
+import org.example.model.Person;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )    {
+        Configuration configuration = new Configuration()
+                .addAnnotatedClass(Person.class).addAnnotatedClass(Passport.class);
+
+        SessionFactory sessionFactory = configuration.buildSessionFactory();
+        Session session = sessionFactory.getCurrentSession();
+
+        try {
+            session.beginTransaction();
+
+            Person person = new Person("Test person", 50);
+            Passport passport = new Passport(person, 123456);
+
+            person.setPassport(passport);
+
+            session.save(person);
+
+            session.getTransaction().commit();
+        } finally {
+            sessionFactory.close();
+        }
+    }
+}
